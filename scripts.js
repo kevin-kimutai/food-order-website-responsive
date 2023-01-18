28568c32d442d95f78a4cc35c41db4e863727270
@kimtai <tezkim414@gmail.com>
2023-01-19 00:13:55 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.querySelector(".hamburger").addEventListener("click", function toggleNav(){
+    document.querySelector(".menu-links").classList.toggle("menu-links:active");
+    document.querySelector(".hamburger").classList.toggle("close-hamburger");
+});
+
+
+
+
+
+
