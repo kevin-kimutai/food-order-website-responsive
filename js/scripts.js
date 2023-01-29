c935a6632482d2630b45537b61fba24f2ca525e9
@kimtai <tezkim414@gmail.com>
2023-01-29 16:04:05 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-document.querySelector(".hamburger").addEventListener("click", function toggleNav(){
-    document.querySelector(".menu-links").classList.toggle("menu-links:active");
-    document.querySelector(".hamburger").classList.toggle("close-hamburger");
+const check = document.getElementById('check');
+const text = document.getElementById('text');
+checkbox = document.querySelectorAll('.check-box').forEach(function(checkbox){
+  checkbox.addEventListener('click',
+  function (){
+   checkbox = document.getElementById('check');
+   checkbox.style.display = 'none';
+   
+    document.querySelectorAll('.add').forEach(function(text){
+      text.innerText = "Added to order list";
+      text.style.color = 'greenyellow';
+
+    })
+  
 });
-
-
-
-
-
+    
+ });
 
